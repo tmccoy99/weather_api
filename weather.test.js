575d04b57865e4e933d3ef055e0aa53c7486e1ff
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-25 11:07:43 +0000

    Tested #load and #getWeatherData methods using mock client

--- weather.test.js
+++ weather.test.js
@@ -1,12 +1,24 @@
-const classes = require("./weather_client");
+const classes = require("./weather");
 
 let weather;
+const mockClient = {
+  fetchWeatherData: jest.fn(),
+};
 
 beforeEach(() => {
-  weather = new classes.Weather(client);
+  weather = new classes.Weather(mockClient);
 });
 
-it("after loading data for given city, getWeatherData returns the data", () => {
-  weather.load("London");
-  expect(weather.getWeatherData()).toStrictEqual();
+it("load method loads city weather data, getWeatherData returns it", (done) => {
+  mockClient.fetchWeatherData.mockResolvedValueOnce({
+    city: "London",
+    temp: 22,
+  });
+  weather.load("London").then(() => {
+    expect(mockClient.fetchWeatherData).toHaveBeenCalledWith("London");
+    const data = weather.getWeatherData();
+    expect(data.city).toBe("London");
+    expect(data.temp).toBe(22);
+    done();
+  });
 });
